0907c1594534031a51f19fcad60821ca22ad255a
Ben <ben.solomovitch@gmail.com>
2022-01-23 21:18:42 +0200

    FibboBen
    add print

--- fibbo.py
+++ fibbo.py
@@ -4,10 +4,13 @@ def fibbo(x,y,z,c):
     if(c==0):
         c=c+1
         fibbo(x,y,z,c)
+        print(x)
     elif(c==1):
         c=c+1
+        print(y)
         fibbo(x,y,z,c)
     elif(c<z):
+        print(x+y)
         p=x
         x=y
         y=y+p
