30aa45a1166fd9d9f6cecb0c3bd96b9f9dd11113
Yael <yael.meshar@gmail.com>
2022-01-23 22:15:41 +0200

    Meshar text!

--- LastMove.py
+++ LastMove.py
@@ -12,21 +12,50 @@ import itertools
 import traceback
 import abc
 
+
 def Yegor():
     x = input("Enter your name:")
-    print("Hello"+x)
+    print("Hello" + x)
+
 
 def Ezert(x):
     for i in range(1, x):
-        x=x*i
+        x = x * i
     return x
+
 
 def Ezert2(x):
-    if(x>1):
-        return x*Ezert2(x-1)
+    if (x > 1):
+        return x * Ezert2(x - 1)
     return x
 
+
 print(Ezert2(5))
+
+
+def fibonacciYael(x):
+    first = 0
+    second = 1
+    if x == 1:
+        return "0"
+    if x == 2:
+        return "0, 1"
+
+    string1 = "0, 1"
+    for i in range(0, x-2):
+        first, second = second, first+second
+        string1 = string1 + ", " + str(second)
+
+    return string1
+
+print(fibonacciYael(4))
+
+with open("Meshar.txt", "w") as f:
+    f.write(str(2**1_000_000))
+
+with open("Meshar.txt", "r") as f:
+    x = f.read()
+    print(len(x))
 
 
 
