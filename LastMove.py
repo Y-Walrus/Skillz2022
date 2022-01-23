b6010f3ee18bba2481ce3c87a9913d4984224155
Yegor <stolyarskyegor@gmail.com>
2022-01-23 17:12:27 +0200

    Create bot file with description and allowed imports

---
+++ LastMove.py
@@ -0,0 +1,15 @@
+# The purpose of this code is to win the Skillz 2022 coding competition
+# Creators: Ben Solomovitch, Ron Bar-El, Yael Meshar, Yegor Stolyarsky
+# 01.2022 - ?
+
+# modules we are allowed to import
+import collections
+import operator
+import random
+import re
+import math
+import itertools
+import traceback
+import abc
+
+# note functions we are not allowed to use
