7c91a26233dd9f5f231652a2922056ab95c50fb6
Yegor <stolyarskyegor@gmail.com>
2022-01-24 14:33:04 +0200

    Setup of game module and Python2.7

--- MyBot.py
+++ MyBot.py
@@ -3,6 +3,8 @@
 # 01.2022 - ?
 
 # modules we are allowed to import, don't add other imports!!
+
+from penguin_game import *
 import collections
 import operator
 import random
@@ -11,4 +13,16 @@ import math
 import itertools
 import traceback
 import abc
+
+
 # note that there are functions we are not allowed to use
+
+
+def do_turn(game):
+    """
+    Makes the bot run a single turn
+
+    :param game: the current game state
+    :type game: Game
+    """
+    print("hi")
